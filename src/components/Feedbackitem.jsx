9b9612bbc12e4edfb0e70befa57fbee67ffc54ab
Njoku Godswill Uchendu Uchechi <godswilluchendu7@gmail.com>
2022-07-06 15:35:08 -0700

    added "useState " in my component

--- src/components/Feedbackitem.jsx
+++ src/components/Feedbackitem.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import {useState} from 'react'
 
 function Feedbackitem() {
+    const [rating, setRating] = useState(7)
+    const [text, setText] = useState('This is an example of a feedback item')
+    const handleClick = () => {
+        setRating((prev) => prev + 1)
+    }
+
   return (
     <div className="card">
-        <div className='num-display'>10</div>
-        <div className='text-dispaly'>This is an example of a feedback item</div>
+        <div className='num-display'>{rating}</div>
+        <div className='text-dispaly'>{text}</div>
+        <button onClick={handleClick}>Click me!!!</button>
     </div>
   )
 }
